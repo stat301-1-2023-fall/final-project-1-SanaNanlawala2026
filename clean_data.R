de851dd72e4244ec5bc2624c085674c6284d474e
Sana Nanlawala <sanananlawala2026@u.northwestern.edu>
2023-11-21 04:53:55 -0600

    cleaned more, fixed bar attempts, etc

--- clean_data.R
+++ clean_data.R
@@ -4,12 +4,12 @@ bar_pass_prediction <- read_csv("data/bar_pass_prediction.csv")
 raw <- bar_pass_prediction
 #write_csv(raw, "raw.csv")
 clean <- bar_pass_prediction |> 
-  select(ID, gender, DOB_yr, ugpa, lsat, grad, fulltime, fam_inc, tier, race1, sex, pass_bar)
+  select(ID, gender, DOB_yr, ugpa, lsat, grad, fulltime, fam_inc, tier, race1, pass_bar, bar)
 
 #Kept Variables:
 
 #ID, keeps each person unique, might remove later
-#Gender, distinguishes between male/female, will cross-reference with sex (1=female, 2=male).
+#Gender, distinguishes between male/female.
 #DOB_yr, all in the 1900s, some outliers older than the 1920s. 
 #ugpa, undergrad gpa
 #lsat, score that was scaled (needs to be changed back to raw lsat score)
@@ -18,7 +18,6 @@ clean <- bar_pass_prediction |>
 #fam_inc, separated by tiers (1-5). What is highest? 5?
 #tier, what tier law school did the student attend by quintile
 #race1, accurate assessment of race without the extra, unnecessary columns,
-#sex, assigns number to sex, only keeping for cross-reference with gender,
 #pass_bar, most important for analysis.
 
 #research was done on this study: the data collected was on students starting law school in 1991.
@@ -35,4 +34,21 @@ clean$fulltime <- ifelse(clean$fulltime == 1, "yes", "no")
 
 clean$pass_bar <- ifelse(clean$pass_bar == 1, "yes", "no")
 
+unique(clean$bar)
+# "a Passed 1st time" ; "c Failed" ; "b Passed 2nd time" ; "e non-Grad"
+
+clean <- clean |> 
+  mutate(
+    bar = fct_recode(bar,
+                         "first attempt" = "a Passed 1st time",
+                         "no pass" = "c Failed",
+                         "second attempt" = "b Passed 2nd time",
+                         "no pass" = "e non-Grad"
+    )) |> 
+  rename(bar_passed_attempt = bar)
+  
+
+unique(clean$grad)
+# Y" "X" NA "O"
+clean$grad <- ifelse(clean$grad == "Y", "yes", "no")
 
