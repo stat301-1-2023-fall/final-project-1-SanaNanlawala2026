6e4f706eef0e28f66f61dc285179b8085af35614
Sana Nanlawala <sanananlawala2026@u.northwestern.edu>
2023-11-22 19:07:24 -0600

    Worked more on different analysis w/explanations

--- plots/Multivariate_Analysis.R
+++ plots/Multivariate_Analysis.R
@@ -1,5 +1,6 @@
 
-
+library(tidyverse)
+library(knitr)
 #States that do not require you to GO to law school to take the bar exam: 
 #California, Vermont, Virginia, Washington
 
@@ -10,4 +11,37 @@
 grad_bar_cleaned <- na.omit(clean[c("pass_bar", "grad")])
 grad_bar_cleaned |> 
 ggplot(aes(x = grad, fill = pass_bar)) +
-  geom_bar(position = "dodge", color = "black")
+  geom_bar(position = "dodge")
+ggsave("plots/grad_bar_distribution.png")
+
+grad_bar_cleaned |> 
+  group_by(pass_bar,grad) |> 
+  summarize(count = n()) |> 
+  kable()
+
+#family income and law school tier
+#law school tiers (1 is the highest) is established by clusers 1-6 with ugpas and lsat scores
+#highest to lowest. 
+fam_inc_tier <- na.omit(clean[c("fam_inc", "law_school_tier")])
+
+fam_inc_tier |> 
+  group_by(fam_inc)  |> 
+  summarize(count = n()) |> 
+  kable()
+
+fam_inc_tier |> 
+  group_by(law_school_tier)  |> 
+  summarize(count = n())
+
+fam_inc_tier |> 
+  group_by(law_school_tier,fam_inc) |> 
+  summarize(count = n()) |> 
+  print(n = 30)
+
+fam_inc_tier |> 
+  ggplot(aes(x = fam_inc)) +
+  geom_histogram() +
+  facet_wrap(~law_school_tier)
+ggsave("plots/fam_inc_tier_distribution.png")
+
+
