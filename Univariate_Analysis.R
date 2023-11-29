25ceb071789d786e4c3625336d58cf4f18aee4f8
Sana Nanlawala <sanananlawala2026@u.northwestern.edu>
2023-11-29 14:07:17 -0600

    fixed plots, made more, did more research

--- Univariate_Analysis.R
+++ Univariate_Analysis.R
@@ -3,31 +3,49 @@ clean <- read_csv("data/clean.csv")
 
 #race
 clean |> 
+  drop_na(race) |> 
   ggplot(aes(x = race))+
-  geom_bar(fill = "pink", color = "black")
+  geom_bar(fill = "blue", color = "black") +
+  geom_text(stat = "count", aes(label = after_stat(count)), vjust = -0.5) +
+  labs(title = "Law School Entrants in 1991: Race",
+       x = "Race",
+       y = "Number of Students")
 ggsave("plots/race_distribution.png")
 #pass bar?
 clean |> 
   ggplot(aes(x = pass_bar)) +
-  geom_bar()
+  geom_bar() +
+  geom_text(stat = "count", aes(label = after_stat(count)), vjust = -0.5) +
+  labs(title = "Law School Entrants in 1991: Pass Rates",
+       x = "Bar Exam Passed",
+       y = "Number of Students")
 ggsave("plots/pass_bar_distribution.png")
 
 #birthyear
 clean |> 
   filter(birth_year > 1940) |> 
   ggplot(aes(x = birth_year)) +
-  geom_histogram(binwidth = 5, fill = "blue", color = "black")
+  geom_histogram(binwidth = 1, fill = "purple", color = "black") +
+  labs(title = "Law School Entrants in 1991: Birth Year",
+       x = "Birth Year",
+       y = "Number of Students")
+ggsave("plots/birth_year_distribution.png")
 
 clean |> 
   filter(birth_year > 1940) |> 
   ggplot(aes(x = birth_year)) +
-  geom_bar( fill = "blue", color = "black")
-ggsave("plots/birth_year_distribution.png")
+  geom_bar(fill = "blue", color = "black")
 
-#graduate law school
+
+#graduate law school 
 clean |> 
+  drop_na(grad) |> 
   ggplot(aes(x = grad))+
-  geom_bar()
+  geom_bar() +
+  geom_text(stat = "count", aes(label = after_stat(count)), vjust = -0.5) +
+  labs(title = "Law School Entrants in 1991: Graduation Rate",
+       x = "Law School Graduate",
+       y = "Number of Students")
 ggsave("plots/grad_distribution.png")
 
 #family income
@@ -35,5 +53,27 @@ clean |>
   ggplot(aes(x = fam_inc))+
   geom_bar()
 ggsave("fam_inc/grad_distribution.png")
+
+#gender
+clean |> 
+  ggplot(aes(x = gender))+
+  geom_bar()
+
+#ugpa
+clean |> 
+  ggplot(aes(x = ugpa))+
+  geom_bar()
+
+#as a density plot
+clean |> 
+  ggplot(aes(x = ugpa))+
+  geom_density(fill = "pink")
+
+clean |> 
+  summarise(
+    "Mean UGPA" = mean(ugpa),
+    "Highest UGPA" = max(ugpa),
+    "Lowest UGPA" = min(ugpa)) |> 
+  kable()
 
 
--- plots/Multivariate_Analysis.R
+++ plots/Multivariate_Analysis.R
@@ -4,17 +4,24 @@ library(knitr)
 
 clean <- read_csv("data/clean.csv")
 raw<- read_csv("data/raw.csv")
+
+
+#grad vs pass_bar ----
+
 #States that do not require you to GO to law school to take the bar exam: 
 #California, Vermont, Virginia, Washington
 
 #States that require SOME law school (applicable to this dataset):
 #New York, Maine, West Virginia
-
-#grad vs pass_bar
 grad_bar_cleaned <- na.omit(clean[c("pass_bar", "grad")])
 grad_bar_cleaned |> 
 ggplot(aes(x = grad, fill = pass_bar)) +
-  geom_bar(position = "dodge")
+  geom_bar(position = "dodge") +
+  geom_text(stat = "count", aes(label = after_stat(count)), position = position_dodge(width = 0.9), vjust = -0.5) +
+  labs(title = "Distribution of Law School Graduation Status and Bar Exam Pass Rates",
+       x = "Law School Graduate",
+       y = "Number of Students",
+       fill = "Bar Exam Passed")
 ggsave("plots/grad_bar_distribution.png")
 
 grad_bar_cleaned |> 
@@ -22,7 +29,7 @@ grad_bar_cleaned |>
   summarize(count = n()) |> 
   kable()
 
-#family income and law school tier
+#family income and law school tier ----
 #law school tiers (1 is the highest) is established by clusers 1-6 with ugpas and lsat scores
 #highest to lowest. 
 fam_inc_tier <- na.omit(clean[c("fam_inc", "law_school_tier")])
@@ -49,7 +56,7 @@ fam_inc_tier |>
 ggsave("plots/fam_inc_tier_distribution.png")
 
 
-#Race vs. Family Income
+#Race vs. Family Income ----
 race_fam_inc <- na.omit(clean[c("fam_inc", "race")])
 
 race_fam_inc |> 
@@ -57,10 +64,33 @@ race_fam_inc |>
   geom_boxplot()
 ggsave("plots/race_fam_inc_distribution.png")
 
-# fulltime vs. fam_income
+# fulltime vs. fam_income ----
 fulltime_fam_inc <- na.omit(clean[c("fam_inc", "fulltime")])
 
 fulltime_fam_inc |> 
   ggplot(aes(x = fulltime, y = fam_inc)) +
   geom_boxplot()
+
+
+## trying with a grouped bar plot
+fulltime_fam_inc |> 
+  ggplot(aes(x = fam_inc, fill = fulltime)) +
+  geom_bar(position = "dodge", color = "white")+
+  coord_flip()
+
+##stacked bar plot THIS VERSION 
+fulltime_fam_inc |> 
+  ggplot(aes(x = fam_inc, fill = fulltime)) +
+  geom_bar(position = "stack", color = "white")+
+  coord_flip()
 ggsave("plots/fulltime_fam_inc.png")
+##insert proportion chart
+
+# lsat scores vs. ugpa ----
+
+#lsat scoes from 1981 (?) to 1991 were on a 10-48 point scale. It is not worth it to scale/convert to today's scales since the tests have gotten much harder.
+
+clean |> 
+  ggplot(aes(x = birth_year, y = lsat))+
+  geom_jitter(alpha = 0.5) +  
+  geom_smooth(method = "lm", se = FALSE, color = "blue")
